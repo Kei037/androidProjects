7eb25b239352777c8db9c8393e5de3798deebee3
Kei037 <juneway@hanmail.net>
2024-05-20 11:36:55 +0900

    android study commit

--- view_binding_ex01/app/src/main/java/com/kei037/view_binding_ex01/MainActivity.kt
+++ view_binding_ex01/app/src/main/java/com/kei037/view_binding_ex01/MainActivity.kt
@@ -2,10 +2,25 @@ package com.kei037.view_binding_ex01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.kei037.view_binding_ex01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // 바인딩 객체 흭득
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // 액티비티 화면 출력
+        setContentView(binding.root)
+
+        // 뷰 객체 이용
+        binding.visibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.VISIBLE
+        }
+        binding.invisibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.INVISIBLE
+        }
     }
 }
